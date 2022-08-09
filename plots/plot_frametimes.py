b5cfb5212d7219f0b3257738602f04d56d582359
Luis Carlos Catalá Martínez <luis@Luiss-MacBook-Pro-2.local>
2022-08-09 19:05:52 +0200

    Memory usage

--- plots/plot_frametimes.py
+++ plots/plot_frametimes.py
@@ -4,8 +4,8 @@ import numpy as np
 import os
 
 
-resolutions = [(1280, 720), (1920, 1080), (2560, 1440), (3840, 2160), (7680, 4320)]
-models = ['bmw', 'viking_room', 'sponza', 'hairball']
+resolutions = [(1280, 720), (1920, 1080), (2560, 1440), (3840, 2160)]
+models = ['bmw', 'sponza', 'hairball']
 
 def plot_frametimes(filename):
     filename = f'{filename}-frametimes.txt'
@@ -60,8 +60,9 @@ def plot_raytraced():
 
 def read_value(filename):
     with open(filename) as f:
-        value = int(f.readline())
+        value = float(f.readlines()[-1])
         return value
+
 def plot_compared_memory_usages():
     rasterized_path = 'VulkanRasterized2070super'
     raytraced_path = 'VulkanRaytraced2070super'
@@ -91,8 +92,69 @@ def plot_compared_memory_usages():
 
     fig.tight_layout()
     plt.savefig('vulkan-memory-usage-comparison.png')
+
+def plot_compared_raytraced_memory_usages():
+    optix_path = 'optix'
+    vulkan_path = 'VulkanRaytraced2070super'
+    for model in models:
+        optix_memory_usages = []
+        vulkan_memory_usages = []
+        for w, h in resolutions:
+            filename = os.path.join(optix_path, f'optix-{w}x{h}{model}-shadows-memoryusage.txt')
+            optix_memory_usages.append(read_value(filename))
+
+            filename = os.path.join(vulkan_path, f'vulkan-{w}x{h}{model}-textures-memoryusage.txt')
+            vulkan_memory_usages.append(read_value(filename))
+
+        labels = [f'{x[0]}x{x[1]}' for x in resolutions]
+        x = np.arange(len(labels))
+        bar_width = 0.35
+        fig, ax = plt.subplots()
+        rects2 = ax.bar(x + bar_width/2, vulkan_memory_usages, bar_width, label='Vulkan')
+        rects1 = ax.bar(x - bar_width/2, optix_memory_usages, bar_width, label='OptiX')
+
+        ax.set_ylabel('Memory (MB)')
+        ax.set_title(f'Memory usage for raytraced rendering of {model} model')
+        ax.set_xticks(x, labels)
+        ax.legend()
+        ax.bar_label(rects1, padding=3)
+        ax.bar_label(rects2, padding=3)
+
+        fig.tight_layout()
+        plt.savefig(f'{model}-memory-usage-comparison.png')
+
+def plot_compared_frame_times():
+    rasterized_path = 'VulkanRasterized2070super'
+    raytraced_path = 'VulkanRaytraced2070super'
+    model = 'viking_room'
+    rasterized_frametimes = []
+    raytraced_frametimes = []
+    for w, h in resolutions:
+        filename = os.path.join(rasterized_path, f'vulkan-{w}x{h}-rasterized-frametimes.txt')
+        rasterized_frametimes.append(read_value(filename))
+
+        filename = os.path.join(raytraced_path, f'vulkan-{w}x{h}{model}-textures-frametimes.txt')
+        raytraced_frametimes.append(read_value(filename))
+
+    labels = [f'{x[0]}x{x[1]}' for x in resolutions]
+    x = np.arange(len(labels))
+    bar_width = 0.35
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - bar_width/2, rasterized_frametimes, bar_width, label='Rasterized')
+    rects2 = ax.bar(x + bar_width/2, raytraced_frametimes, bar_width, label='Raytraced')
+
+    ax.set_ylabel('Frame time (microseconds)')
+    ax.set_title('Frame render times for rasterized and raytraced rendering')
+    ax.set_xticks(x, labels)
+    ax.legend()
+    ax.bar_label(rects1, padding=3)
+    ax.bar_label(rects2, padding=3)
+
+    fig.tight_layout()
+    plt.savefig('vulkan-frametimes-comparison.png')
 if __name__ == '__main__':
     # plot_rasterized()
     # plot_raytraced()
-    plot_compared_memory_usages()
-
+    # plot_compared_memory_usages()
+    # plot_compared_frame_times()
+    plot_compared_raytraced_memory_usages()
